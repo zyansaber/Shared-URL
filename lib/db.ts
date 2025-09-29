28dfd98a164b9827c1177d145666e3c8edc0fed1
Done-jpg <dongning@regentrv.com.au>
2025-09-29 14:35:25 +1000

    remove

--- lib/db.ts
+++
@@ -1,8 +0,0 @@
-import sql from 'mssql'
-let pool: sql.ConnectionPool | null = null
-export async function getPool() {
-  if (pool && pool.connected) return pool
-  pool = await sql.connect(process.env.DATABASE_URL as string)
-  return pool
-}
-export { sql }
